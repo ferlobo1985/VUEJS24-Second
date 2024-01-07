aade1a8b4d386cf79676c6d51889f2a6ce67539e
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-07 02:11:20 -0300

    02-Creating a VUE app
    02-Creating a VUE app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+    const name = ref('Francis');
+</script>
+
+<style>
+    div {
+        color:red
+    }
+</style>
