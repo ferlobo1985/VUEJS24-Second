75735476fddd20d5a3e88876d0859080bdff3273
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 05:06:54 -0300

    16-Keep alive
    16-Keep alive

--- src/components/Players/mike.vue
+++ src/components/Players/mike.vue
@@ -1,3 +1,20 @@
 <template>
     <h3>Mike is on</h3>
 </template>
+
+<script setup>
+    import { onUnmounted, onActivated, onDeactivated  } from 'vue'
+
+
+    onActivated(()=>{
+        console.log('x-onActivated')
+    })
+
+    onDeactivated(()=>{
+        console.log('o-onDeactivated')
+    })
+
+    onUnmounted(()=>{
+        console.log('Mike unmounted')
+    })
+</script>
--- src/components/Players/steve.vue
+++ src/components/Players/steve.vue
@@ -1,3 +1,11 @@
 <template>
     <h3>Steve is on</h3>
 </template>
+
+<script setup>
+    import { onUnmounted } from 'vue'
+
+    onUnmounted(()=>{
+        console.log('Steve unmounted')
+    })
+</script>
