0a6b795f232e17fbdddbd7b852b8fc0a85f26b85
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 05:43:35 -0300

    19-CD hooks
    19-CD hooks

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,9 +1,12 @@
 <template>
     <div v-awesome.red.big="textToRender"></div>
-    <div v-awesome.blue.small="textToRender"></div>
+    <!-- <div v-awesome.blue.small="textToRender"></div> -->
+    <button @click="textToRender = 'Someting else'">
+        Trigger update
+    </button>
 </template>
 
 <script setup>
     import {ref} from 'vue';
-    const textToRender = ref('Some text')
+    const textToRender = ref('Some text');
 </script>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,32 @@ import Header from '@/components/header_footer/header.vue';
 const app = createApp(App);
 
 app.directive('awesome',{
+    created(el){
+        console.log('Created')
+        console.log(el.parentNode)
+    },
     beforeMount(el, binding){
         el.style.color = binding.modifiers.red ? 'red':'blue';
         el.style.fontSize = binding.modifiers.big ? '20px':'10px';
-
         el.innerHTML = binding.value;
-    }
+
+        console.log('beforeMount')
+        console.log(el.parentNode)
+    },
+    mounted(el){
+        console.log('mounted')
+        console.log(el.parentNode)
+    },
+    beforeUpdate(el,binding){
+        console.log('beforeUpdate')
+        console.log(el.parentNode)
+        el.innerHTML = binding.value;
+    },
+    updated(){
+        console.log('UPDATED')
+    },
+    beforeUnmount(){},
+    unmounted(){}
 })
 app.component('app-header',Header)
 app.mount('#app')
