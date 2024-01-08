e3143ea774b5b79f2c0d88c2027e2e63c62cb9f2
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 05:17:14 -0300

    17-Teleport
    17-Teleport

--- src/components/Players/steve.vue
+++ src/components/Players/steve.vue
@@ -1,9 +1,13 @@
 <template>
     <h3>Steve is on</h3>
+    <Teleport to="#dialog_here">
+        <Modal/>
+    </Teleport>
 </template>
 
 <script setup>
-    import { onUnmounted } from 'vue'
+    import Modal from '@/components/Utils/modal.vue'
+    import { onUnmounted } from 'vue';
 
     onUnmounted(()=>{
         console.log('Steve unmounted')
