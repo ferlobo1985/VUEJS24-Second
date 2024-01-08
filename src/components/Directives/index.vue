71717d3de9bc00373293065cfce5e7b36be2663c
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 05:51:18 -0300

    20-Local CD
    20-Local CD

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div v-awesome.red.big="textToRender"></div>
+    <div v-user-lastname="lastname"></div>
     <!-- <div v-awesome.blue.small="textToRender"></div> -->
     <button @click="textToRender = 'Someting else'">
         Trigger update
@@ -9,4 +10,12 @@
 <script setup>
     import {ref} from 'vue';
     const textToRender = ref('Some text');
+    const lastname = ref('jones')
+
+    const vUserLastname = {
+        beforeMount(el,binding){
+            el.innerHTML =  binding.value;
+            el.style.color = 'red'
+        }
+    }
 </script>
